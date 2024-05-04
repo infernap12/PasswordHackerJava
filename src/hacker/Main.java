bd00f1a33c6eb405983597d84d46990c840c9d5b
james <james1.6@hotmail.com>
2024-05-05 01:03:21 +0930

    Pass Stage 2
    Add password generation functionality
    
    Updated the main class to include a function for password generation. The number of arguments required for the main function was revised. Within the main try block, a loop was added to generate passwords of increasing length until a successful connection message is received.

--- src/hacker/Main.java
+++ src/hacker/Main.java
@@ -3,24 +3,34 @@ package hacker;
 import java.io.IOException;
 import java.net.Socket;
 import java.net.UnknownHostException;
+import java.util.Objects;
 
 public class Main {
 
     static NetClient client;
+    private static String pw = null;
 
     public static void main(String[] args) {
-        if (args.length != 3) {
+        if (args.length != 2) {
             System.out.println("Invalid number of arguments!");
             System.exit(1);
         }
         String address = args[0];
         int port = Integer.parseInt(args[1]);
-        String msg = args[2];
 
 
         try (Socket socket = new Socket(address, port)) {
             client = new NetClient(socket);
-            System.out.println(client.sendAndReceive(msg));
+            String response = "";
+
+
+            for (int maxLength = 0; maxLength < 5; maxLength++) {
+                generate("", maxLength);
+            }
+
+            System.out.println(pw);
+
+
         } catch (UnknownHostException e) {
             throw new RuntimeException(e);
         } catch (IOException e) {
@@ -28,5 +38,33 @@ public class Main {
         }
 
 
+    }
+
+    public static void generate(String current, int maxLength) {
+        if (current.length() == maxLength) {
+            if (pw != null) {
+                return;
+            }
+            if (Objects.equals(client.sendAndReceive(current), "Connection success!")) {
+                pw = current;
+            }
+            return;
+        }
+
+        // Loop through 'a' to 'z'
+        for (char c = 'a'; c <= 'z'; c++) {
+            if (pw != null) {
+                return;
+            }
+            generate(current + c, maxLength);
+        }
+
+        // Loop through '0' to '9'
+        for (char c = '0'; c <= '9'; c++) {
+            if (pw != null) {
+                return;
+            }
+            generate(current + c, maxLength);
+        }
     }
 }
